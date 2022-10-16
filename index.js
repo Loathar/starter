2ba2110030e5ba55bd0e296b14d876373396ba29
Tor Henning Egenes <tor.henning.egenes@gmail.com>
2022-10-16 22:52:17 +0200

    progress

---
+++ index.js
@@ -0,0 +1,69 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+///////////////////
+
+// Files
+
+// Blocking, synchronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+
+// const textOut = `This is what we know about the avocado: ${textIn}\n Created on ${Date.now()}`;
+
+// console.log(textOut);
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File has been written!");
+// fs.writeFileSync("./txt/hello.txt", "Hello World!");
+// console.log("File has been written!");
+
+// Non-Blocking, Asynchronous way
+
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) return console.log("ERROR! 💥");
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+//       console.log(data3);
+//       fs.writeFile(
+//         "./txt/final.txt",
+//         `${data2}\n${data3}`,
+//         "utf-8",
+//         (err) => {}
+//       );
+//       console.log("Youre file has been written! 😃");
+//     });
+//   });
+// });
+
+// console.log("Will read file..");
+
+/////////////////
+// SERVER
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObject = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  //console.log(req.url);
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/products") {
+    res.end("This is the PRODUCTS");
+  } else if (pathName === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+    });
+    res.end("<h1>Page not found!</h1> ");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening on port 8000...");
+});
